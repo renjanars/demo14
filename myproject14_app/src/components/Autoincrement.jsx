05782f24623c83be4765b505096fadb5046c0445
renjanars <renjanarskumar@gmail.com>
2024-11-10 19:37:16 +0530

    style applied

--- myproject14_app/src/components/Autoincrement.jsx
+++ myproject14_app/src/components/Autoincrement.jsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 import '../styles/Auto.css';
-
-
-
 function AutoIncrementCounter() {
-  // Initialize state for count
   const [count, setCount] = useState(0);
-
-  // Function to increment count
   const incrementCount = () => {
     setCount(count + 1);
   };
-
-  return (
+return (
     <div className="counter-container">
       <h1>Counter: {count}</h1>
       <button onClick={incrementCount} className="increment-button">
